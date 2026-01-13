fd0ceb9f1923d37480c7e42fa18e8951465be23e
n4vho <n4vho@users.noreply.github.com>
2026-01-12 23:54:44 -0500

    remove scanf weakness and tokenize instead

--- repl.c
+++ repl.c
@@ -114,6 +114,74 @@ typedef struct {
 } Table;
 
 
+void* get_page(Pager* pager, uint32_t page_num) {
+
+    if (page_num > TABLE_MAX_PAGES) {
+        printf("Tried to fetch page numer out of bounds. %d > %d\n", page_num, TABLE_MAX_PAGES);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pager->pages[page_num] == NULL) {
+
+        // cache miss. Allocate memory and load from file 
+        void* page = malloc(PAGE_SIZE);
+        uint32_t num_pages = pager->file_length / PAGE_SIZE;
+        
+        // might save a partial page at the end of the file 
+        if (pager->file_length % PAGE_SIZE) {
+            num_pages += 1;
+        }
+
+        if (page_num <= num_pages) {
+            lseek(pager->file_descriptor, page_num * PAGE_SIZE, SEEK_SET);
+            ssize_t bytes_read = read(pager->file_descriptor, page, PAGE_SIZE);
+
+            if(bytes_read == -1) {
+                printf("Error reading file: %d\n", errno);
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        pager->pages[page_num] = page;
+    }
+
+    return pager->pages[page_num];
+}
+
+
+void db_close(Table* table) {
+    
+    Pager* pager = table->pager;
+    uint32_t num_full_pages = table->num_rows / ROWS_PER_PAGE;
+
+    for (uint32_t i = 0; i < num_full_pages; i++) {
+        if (pager->pages[i] == NULL) {
+            continue;
+        }
+
+        pager_flush(pager, i, PAGE_SIZE);
+        free(pager->pages[i]);
+        pager->pages[i] = NULL;
+    }
+
+    // might be a partial page to write to the EOF 
+    // wont need it anymore after i implement b-tree
+    uint32_t num_additional_rows = table->num_rows % ROWS_PER_PAGE;
+    if (num_additional_rows > 0) {
+        uint32_t page_num = num_full_pages;
+        if(pager->pages[page_num] != NULL) {
+            pager_flush(pager, page_num, num_additional_rows * ROW_SIZE);
+            free(pager->pages[page_num]);
+            pager->pages[page_num] = NULL;
+        }
+    }
+
+    int result = close(pager->file_descriptor);
+    if (result == -1) {
+
+    }
+}
+
 void print_row(Row* row) {
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
@@ -121,11 +189,14 @@ void print_row(Row* row) {
 void* row_slot(Table* table, uint32_t row_num) {
 
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    void* page = table->pages[page_num];
+    /*void* page = table->pages[page_num];
     if (page == NULL) {
         // allocating memory only when accessing page
         page = table->pages[page_num] = malloc(PAGE_SIZE);
-    }
+    }*/ 
+    
+    void* page = get_page(table->pager, page_num);
+
 
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
     uint32_t byte_offset = row_offset * ROW_SIZE;
