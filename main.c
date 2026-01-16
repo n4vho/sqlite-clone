c16092c77bbe2e8d6241cc28ed6880cccc813470
n4vho <n4vho@users.noreply.github.com>
2026-01-15 22:15:02 -0500

    implement a cursor

--- main.c
+++ main.c
@@ -117,6 +117,35 @@ typedef struct {
 } Table;
 
 
+typedef struct {
+    Table* table;
+    uint32_t row_num;
+    bool end_of_table;  // Indicates a position one past the last element
+}Cursor;
+
+
+Cursor* table_start(Table* table) {
+
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = 0;
+    cursor->end_of_table = (table->num_rows == 0);
+
+    return cursor;
+}
+
+
+Cursor* table_end(Table* table) {
+
+    Cursor* cursor = malloc(sizeof(Cursor));
+    cursor->table = table;
+    cursor->row_num = table->num_rows;
+    cursor->end_of_table = true;
+
+    return cursor;
+}
+
+
 void* get_page(Pager* pager, uint32_t page_num) {
 
     if (page_num > TABLE_MAX_PAGES) {
@@ -225,16 +254,12 @@ void print_row(Row* row) {
     printf("(%d, %s, %s)\n", row->id, row->username, row->email);
 }
 
-void* row_slot(Table* table, uint32_t row_num) {
+void* cursor_value(Cursor* cursor) {
 
+    uint32_t row_num = cursor->row_num;
     uint32_t page_num = row_num / ROWS_PER_PAGE;
-    /*void* page = table->pages[page_num];
-    if (page == NULL) {
-        // allocating memory only when accessing page
-        page = table->pages[page_num] = malloc(PAGE_SIZE);
-    }*/ 
     
-    void* page = get_page(table->pager, page_num);
+    void* page = get_page(cursor->table->pager, page_num);
 
 
     uint32_t row_offset = row_num % ROWS_PER_PAGE;
@@ -242,6 +267,18 @@ void* row_slot(Table* table, uint32_t row_num) {
 
     return page + byte_offset;
 }
+
+
+void cursor_advance(Cursor* cursor) {
+    cursor->row_num += 1;
+
+    if(cursor->row_num >= cursor->table->num_rows) {
+        cursor->end_of_table = true;
+    }
+}
+
+
+
 
 void serialize_row(Row* source, void* destination) {
 
@@ -330,21 +367,29 @@ Execute_Result execute_insert(Statement* statement, Table* table) {
     }
 
     Row* row_to_insert = &(statement->row_to_insert);
+    Cursor* cursor = table_end(table);
 
-    serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    // serialize_row(row_to_insert, row_slot(table, table->num_rows));
+    serialize_row(row_to_insert, cursor_value(cursor));
     table->num_rows += 1;
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
 
 Execute_Result execute_select(Statement* statement, Table* table) {
     
+    Cursor* cursor = table_start(table);
     Row row;
     
-    for(uint32_t i = 0; i < table->num_rows; i++) {
-        deserialize_row(row_slot(table, i), &row);
+    while (!(cursor->end_of_table)) {
+        deserialize_row(cursor_value(cursor), &row);
         print_row(&row);
+        cursor_advance(cursor);
     }
+
+    free(cursor);
 
     return EXECUTE_SUCCESS;
 }
